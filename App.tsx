460a184720b83a9a8c509e26cac8632d0a9f2677
wens <2182908911@qq.com>
2024-12-09 10:36:39 +0800

    feat:finish code init work

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, ScrollView, Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Text>App</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
